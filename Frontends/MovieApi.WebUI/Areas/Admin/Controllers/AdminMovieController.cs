8b53eedfafab718548e385a56539490fafd8385e
MuratYucedag <yucedagmurat23@gmail.com>
2025-12-18 17:23:49 +0300

    ...

--- Frontends/MovieApi.WebUI/Areas/Admin/Controllers/AdminMovieController.cs
+++ Frontends/MovieApi.WebUI/Areas/Admin/Controllers/AdminMovieController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MovieApi.Dto.Dtos.AdminCategoryDtos;
 using MovieApi.Dto.Dtos.AdminMovieDtos;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace MovieApi.WebUI.Areas.Admin.Controllers
 {
@@ -21,6 +23,26 @@ namespace MovieApi.WebUI.Areas.Admin.Controllers
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<AdminResultMovieDto>>(jsonData);
                 return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateMovie()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMovie(CreateAdminMovieDto createAdminMovieDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createAdminMovieDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7053/api/Movies", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("MovieList");
             }
             return View();
         }
